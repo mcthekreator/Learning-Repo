9690d0cd331ddcd81e42685ae3a8e4b2db022e02
mcadamsx <you@example.com>
2024-07-24 19:52:34 +0000

    initial commit

---
+++ 05. React/firstPro/src/components/Hello.jsx
@@ -0,0 +1,10 @@
+function Hello(props) {
+    return (
+        <>
+        <h1>{props.message} {props.name} </h1>
+        </>  
+    );
+       
+}
+ 
+export default Hello;
---
+++ 09. Typescript/Full course/Classes/shopping-app/src/App.tsx
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+import './App.css';
+import ShoppingList from './components/ShoppingList';
+
+
+function App(): JSX.Element {
+
+ 
+
+  const items = [
+    {id:1, product: "Lemon", quantity:3 },
+    {id:2, product: "Chicken Breast", quantity:2 }
+  ]
+
+  return (
+    <div >
+      {<ShoppingList items={items}/>}
+    </div>
+  );
+}
+
+export default App;
