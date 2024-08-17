30e9e76385e4c9a39f6017916563afb8f59cbd66
Richard McAdams <mcadamsrd@gmail.com>
2024-08-17 22:12:23 +0000

    python: conditional statement

--- 05. React/code-stoic/src/App.jsx
+++ 05. React/code-stoic/src/App.jsx
@@ -2,11 +2,10 @@ import Fruits from "./components/fruits";
 import Hello from "./components/Hello";
 
 function App() {
-
   return (
     <>
-      <Hello name="Derrick" message="Hi ther"/>
-      <Fruits/>
+      <Hello name="Derrick" message="Hi ther" />
+      <Fruits />
     </>
   );
 }
--- 05. React/code-stoic/src/components/fruits.jsx
+++ 05. React/code-stoic/src/components/fruits.jsx
@@ -1,12 +1,10 @@
 export default function Fruits() {
-    const fruits = ["Apple", "Mango", "Banana", "Orange"];
-  
-    return (
-      <>
-        {fruits.map((fruit) => (
-          <h3 key={fruit}>{fruit}</h3>
-        ))}
-      </>
-    );
-  }
-  
+  const fruits = ["Apple", "Mango", "Banana", "Orange"];
+  return (
+    <>
+      {fruits.map((fruit) => (
+        <h3 key={fruit}>{fruit}</h3>
+      ))}
+    </>
+  );
+}
