9a0bbba6db97f394c2cde2758e3defe2b173a6fa
Richard McAdams <mcadamsrd@gmail.com>
2024-08-18 21:55:38 +0000

    Python: Variables

--- 07.Python/script.py
+++ 07.Python/script.py
@@ -1,21 +1,27 @@
-# variables
-# declaring variables
-miles = 10
-# declareing mulitple variables
-a, b, c = 0, 1, 3
-print(a, b, c)
-miles = "Derrick"
-miles.replace("Richard", "Derrick")
-print(miles)
+# # variables
+# # declaring variables
+# miles = 10
+# # declareing mulitple variables
+# a, b, c = 0, 1, 3
+# print(a, b, c)
+# miles = "Derrick"
+# miles.replace("Richard", "Derrick")
+# print(miles)
 
-if miles == "Derrick":
-    print("The variable miles is now Derrick")
-else:
-    print("The variable miles is now")
+# if miles == "Derrick":
+#     print("The variable miles is now Derrick")
+# else:
+#     print("The variable miles is now")
 
 
-# def long_lop(a=33, b=33):
-#     if b > a:
-#         print("b is greater than a")
-#     elif a == b:
-#         print("a and b are equal")
+# # def long_lop(a=33, b=33):
+# #     if b > a:
+# #         print("b is greater than a")
+# #     elif a == b:
+# #         print("a and b are equal")
+
+student_count = 100 
+rating = 4.99
+is_published = False
+course_name = """ Mulitple Lines """
+print(course_name)
