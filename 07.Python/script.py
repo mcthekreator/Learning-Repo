1891868087d2ef18a4e8abd835af942b587227f8
Richard McAdams <mcadamsrd@gmail.com>
2024-08-24 23:17:16 +0000

    python: addition of two numbers

--- 07.Python/script.py
+++ 07.Python/script.py
@@ -148,3 +148,12 @@
 
 meaning = 42
 print(' ')
+
+
+# Additionof two numebers
+
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+sum_of_numbers = num1 + num2
+print(f"{num1} + {num2} = {sum_of_numbers}")
