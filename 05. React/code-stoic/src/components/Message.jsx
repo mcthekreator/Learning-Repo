4de5030c34d83cb29adf065e89925b55a043f2a8
Richard McAdams <mcadamsrd@gmail.com>
2024-08-31 21:48:38 +0000

    react: event handling

--- 05. React/code-stoic/src/components/Message.jsx
+++ 05. React/code-stoic/src/components/Message.jsx
@@ -1,12 +1,14 @@
 
 export default function Message() {
+    let showMesage = false
     function handleClick() {
-        alert("Button clicked");
+        showMesage = true
 
     }
     return (
         <div>
             <button onClick={handleClick} >Click here ti get a message</button>
+            {showMesage ? <h1>Hello </h1> : <h1>Mesage</h1>}
         </div>
     )
 }
