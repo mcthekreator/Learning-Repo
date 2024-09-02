b0a5de5e465ac90894bf43ecd1dec90adcb1b4fe
Richard McAdams <mcadamsrd@gmail.com>
2024-09-02 19:13:42 +0000

    react: usesate

--- 05. React/code-stoic/src/components/Counter.jsx
+++ 05. React/code-stoic/src/components/Counter.jsx
@@ -2,11 +2,20 @@ import React, { useState } from 'react'
 
 export default function Counter() {
     const [count, setCount] = useState(0)
+    function handleClick(){
+        setCount(count + 1)
+    }
+    function decreaseClick(){
+        setCount(count - 1)
+    }
+
     
 
     return (
         <div>
-            Count value is: 
+            <h1>Count value is: {count}</h1>
+            <button onClick={handleClick}>increment</button>
+            <button onClick={decreaseClick}>decrement</button>
         </div>
     )
 }
