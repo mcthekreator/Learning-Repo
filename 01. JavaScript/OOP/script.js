849fbe7a09465829bc296b05a9424e37ed56836f
Richard McAdams <mcadamsrd@gmail.com>
2024-09-09 10:58:01 +0000

    js: factory fuction

---
+++ 01. JavaScript/OOP/script.js
@@ -0,0 +1,49 @@
+// Object Oreineted Programming
+
+// This is procedure programming 
+let baseSalary  = 30.00;
+let overTime = 10.00;
+let rate = 20;
+function getWage (baseSalary, overTime, rate){
+    return baseSalary + (overTime * rate)
+}
+
+// This is encapsulation in OOP
+let  employee = {
+    baseSalary: 30.00,
+    overTime: 10.00,
+    rate: 20,
+    getWage: function(){
+        return this.baseSalary + (this.overTime * this.rate)
+    }
+}
+
+
+// Object Literals
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    draw: function() {
+        console.log('draw');
+    }
+}
+
+// and object with one or more methods is said to have behaviour
+// Behaviour is the actions and operations that an object can perform  
+
+// factory function
+function createCircle(radius){
+    return {
+        radius,
+        draw() {
+            console.log('draw');
+        }
+    };
+
+}
+
+const circle1 = createCircle(1);
+circle.draw();
