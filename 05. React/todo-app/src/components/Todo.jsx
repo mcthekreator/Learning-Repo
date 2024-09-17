4f0df21879a2c80548d6492821cd7e1a2ce4b53c
Richard McAdams <mcadamsrd@gmail.com>
2024-09-17 20:02:51 +0000

    react: displaying todos

--- 05. React/todo-app/src/components/Todo.jsx
+++ 05. React/todo-app/src/components/Todo.jsx
@@ -1,20 +1,29 @@
 import { useState } from "react";
+import TodoItem from "./TodoItem";
 export default function Todo() {
   const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
   function handleSubmit(e) {
     e.preventDefault();
-  
-    
+    setTodos([...todos, todo]);
+    setTodo("");
+    console.log(todos);
   }
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        onChange={(e) => setTodo(e.target.value)}
-        value={todo}
-        type="text"
-        placeholder="Add a Todo"
-      />
-      <button type="submit">Add</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+          type="text"
+          placeholder="Add a Todo"
+        />
+        <button type="submit">Add</button>
+      </form>
+
+      {todos.map((item, index) => (
+        <TodoItem item={item} />
+      ))}
+    </div>
   );
 }
---
+++ 05. React/todo-app/src/components/TodoItem.jsx
@@ -0,0 +1,7 @@
+export default function TodoItem(props) {
+  return (
+    <ul>
+      <li>{props.item}</li>
+    </ul>
+  );
+}
