92d5eb209091ea1af9d3ea723fca06765e0075f9
Richard McAdams <mcadamsrd@gmail.com>
2024-09-17 14:03:28 +0000

    react: creating input fields

--- 05. React/todo-app/src/components/Todo.jsx
+++ 05. React/todo-app/src/components/Todo.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react"
+
 export default function Todo() {
+    const [todo, setTodo] = useState("")
+    
     return (
         <form action="">
-            
+            <input onChange={(e) => setTodo(e.target.value)} value={todo} type="text" />
+            <button type="submit">Add</button>
         </form>
     )
 }
