e094c7aed0093226244c3e565232669e4152af92
Richard McAdams <mcadamsrd@gmail.com>
2024-09-22 21:45:44 +0000

    react: update on stick wall

--- 05. React/sticky-wall/src/components/AddNote.jsx
+++ 05. React/sticky-wall/src/components/AddNote.jsx
@@ -3,8 +3,8 @@ export default function AddNote() {
     alert("Form added");
   };
   return (
-    <button onClick={handleClick} className="border bg-transparent border-red-200 h-52 w-full ">
-      Add Note
+    <button onClick={handleClick} className=" bg-gray-200 border-red-200 h-52 w-full text-black text-3xl font-semibold ">
+      Add Note +
     </button>
   );
 }
