ee04b42aa393cb204c5afd67a92ae0ef23af9be8
Richard McAdams <mcadamsrd@gmail.com>
2024-09-23 23:22:22 +0000

    react: added todo form component

--- 05. React/sticky-wall/src/components/AddNote.jsx
+++ 05. React/sticky-wall/src/components/AddNote.jsx
@@ -1,12 +1,13 @@
 import AddNoteForm from "./AddNoteForm";
 
 export default function AddNote() {
+  let display = false;
   const handleClick = () => {
-    alert("Form added");
+    display = true;
   };
   return (
     <div>
-      <AddNoteForm />
+      {display ? <AddNoteForm /> : display}
       <button
         onClick={handleClick}
         className=" bg-gray-200 border-red-200 h-52 w-full text-black text-center text-4xl font-bold "
