1b23cbc1497c83d80c98a5ebdc5ee9ed4b8c1db3
Richard McAdams <mcadamsrd@gmail.com>
2024-09-25 07:47:55 +0000

    react: added todo form component

--- 05. React/sticky-wall/src/components/AddNoteForm.jsx
+++ 05. React/sticky-wall/src/components/AddNoteForm.jsx
@@ -1,9 +1,10 @@
 export default function AddNoteForm() {
-    return (
-       <form action="">
-        <input type="text" placeholder="Title" />
-        <textarea name="" id="" placeholder="Description"></textarea>
-        <button>Add</button>
-       </form>
-    )
+//   let [title, setTitle] = useSate();
+  return (
+    <form action="">
+      <input type="text" placeholder="Title" />
+      <textarea name="" id="" placeholder="Description"></textarea>
+      <button>Add</button>
+    </form>
+  );
 }
