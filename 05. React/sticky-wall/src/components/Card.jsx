3590e145b79a1243ebf2c93c962f51604554a5a3
Richard McAdams <mcadamsrd@gmail.com>
2024-09-29 22:22:05 +0000

    react: adding card componenet for todo

--- 05. React/sticky-wall/src/components/Card.jsx
+++ 05. React/sticky-wall/src/components/Card.jsx
@@ -2,5 +2,8 @@ export default function Card({title, description}) {
   return <div className="border border-red-200 h-52 w-full ">
 
    <h1> {title}</h1>
+   <textarea name="" id="">
+    {description}
+   </textarea>
   </div>;
 }
