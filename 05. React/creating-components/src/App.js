c7deb4496da9c788128b05d431a75d6b63a4d7b2
Richard McAdams <mcadamsrd@gmail.com>
2024-10-01 08:44:44 +0000

    react: starting with freecodecamp react

--- 05. React/creating-components/src/App.js
+++ 05. React/creating-components/src/App.js
@@ -1,12 +1,9 @@
 import "./App.css";
-import FetchBtn from "./components/FetchBtn";
-import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <Header />
-      <FetchBtn/>
+    <h1>Hello World</h1>
     </>
   );
 }
