cbdb5bf7017d7944ef0b236f2c4bd3f8f059ad67
Richard McAdams <mcadamsrd@gmail.com>
2024-10-03 22:16:55 +0000

    stickywall: added form submit funtion

--- 05. React/sticky-wall/src/components/AddNoteForm.jsx
+++ 05. React/sticky-wall/src/components/AddNoteForm.jsx
@@ -1,7 +1,12 @@
 export default function AddNoteForm() {
 //   let [title, setTitle] = useSate();
+
+const addToForm =()=>{
+
+}
+
   return (
-    <form action="">
+    <form action="" onSubmit={addToForm}>
       <input type="text" placeholder="Title" />
       <textarea name="" id="" placeholder="Description"></textarea>
       <button>Add</button>
