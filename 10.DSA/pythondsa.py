4e0e3f483c707ce922cc956903d93ca646b6d32e
Richard McAdams <mcadamsrd@gmail.com>
2024-10-07 08:33:05 +0000

    python: dsa binary search

--- 10.DSA/pythondsa.py
+++ 10.DSA/pythondsa.py
@@ -1,21 +1,35 @@
-def linear_search(list, target):
-    """
-    Returns the index potiiton of the target if found, else returns None
-    """
-    for i in range (0, len(list)):
-        if list[i] == target:
-            return i
-    return None    
+# Linear Search
 
+# def Linear_search (list, target):
+#     for i in range(len(list)):
+#         if list[i] == target:
+#             return i
+#     return None
 
-
-def verify (index):
-    if index is not None:
-        print(f"The target was found at index {index}")
-    else:
-        print("The target was not found in the list")
+# def Verify(index):
+#     if index is not None:
+#         print(f"Element found at index: {index}")
+#     else:
+#         print("Element not found in the list.")
+            
         
-numbers = [1,2,3,4,5,6,7,8,9,10]  
-result = linear_search(numbers, 6)
-verify(result)      
-        
+# numbers = [1, 5, 8, 10, 12, 15, 18, 20, 25]
+# results = Linear_search(numbers,40)
+# Verify(results)
+
+
+# Binary_search
+def binary_sarch(list, target):
+    first = 0
+    last =  len(list) - 1
+    
+    while first <= last:
+        midpoint = (first + last) //2
+        if list[midpoint] == target:
+            return midpoint
+        elif list[midpoint] < target:
+            first = midpoint + 1
+        else:
+            last = midpoint - 1
+            
+            
