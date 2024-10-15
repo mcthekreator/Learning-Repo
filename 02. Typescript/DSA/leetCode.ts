d1fd28178365b251a3671acff5d6523d76d151bc
mcthekreator <mcadamsrd@gmail.com>
2024-10-15 18:51:45 +0000

    typescript: added typescript dsa dicrectory

--- 02. Typescript/DSA/leetCode.ts
+++ 02. Typescript/DSA/leetCode.ts
@@ -14,4 +14,3 @@ function twoSum(nums: number[], target: number): number[] {
     throw new Error("No two sum solution");
 }
 console.log(twoSum([2, 7, 11, 15], 9));
-console.log(twoSum([3, 2, 4], 6));
