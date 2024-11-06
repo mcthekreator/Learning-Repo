1d175a3207bbd287a0ff1575ff1dcf8666b2ef94
mcthekreator <mcadamsrd@gmail.com>
2024-11-06 20:22:31 +0000

    update learning note

--- 11. Angular/lifecyclehooks/src/app/app.module.ts
+++ 11. Angular/lifecyclehooks/src/app/app.module.ts
@@ -3,19 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { ChildComponent } from './componenets/child/child.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [
-    provideAnimationsAsync()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
