21a791ec97c771afab5f1b801a1f2d4519f653e3
mcthekreator <mcadamsrd@gmail.com>
2024-11-07 17:47:33 +0000

    angular: ngxs actions and store

--- 11. Angular/ngxsproj/src/app/app.module.ts
+++ 11. Angular/ngxsproj/src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxsModule } from '@ngxs/store';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxsModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+ 
---
+++ 11. Angular/ngxsproj/src/app/pages/auth/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  public loginForm:FormGroup;
+  private fb = inject(FormBuilder)
+
+  constructor() {
+    this.loginForm = this.fb.group({
+      email: [''],
+      password: ['']
+    });
+  }
+
+  public login(): void {
+    let loginData = this.loginForm.value;
+    console.log(loginData);
+    
+  }
+
+}
