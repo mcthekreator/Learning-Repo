9dd87243b4693a439b1ce4d63b6d0ce76e35233d
mcthekreator <mcadamsrd@gmail.com>
2024-11-11 09:09:00 +0000

    angular: nxs state mgt

--- 11. Angular/ngxsproj/src/app/store/animal.actions.ts
+++ 11. Angular/ngxsproj/src/app/store/animal.actions.ts
@@ -2,3 +2,15 @@ export class AddAnimal{
     static readonly type = "[Zoo] Add Animal" ;
     constructor(){}
 }
+export class DeleteAnimal{
+    static readonly type = "[Zoo] Delete Animal" ;
+    constructor(){}
+}
+export class GetAnimal{
+    static readonly type = "[Zoo] Get Animal" ;
+    constructor(){}
+}
+export class UpdateAnimal{
+    static readonly type = "[Zoo] Update Animal" ;
+    constructor(){}
+}
