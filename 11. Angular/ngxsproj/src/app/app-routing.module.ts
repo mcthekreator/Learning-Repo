3455dd896817011f4e5a5a47e01350947f71fee3
mcthekreator <mcadamsrd@gmail.com>
2024-11-11 10:45:34 +0000

    angular: ngxs state mgt

--- 11. Angular/ngxsproj/src/app/app-routing.module.ts
+++ 11. Angular/ngxsproj/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/auth/login/login.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  // { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent},
   {path: 'home', component: HomeComponent}
 ];
 
--- 11. Angular/ngxsproj/src/app/pages/auth/login/login.component.ts
+++ 11. Angular/ngxsproj/src/app/pages/auth/login/login.component.ts
@@ -1,14 +1,19 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AddAnimal } from '../../../store/animal.actions';
+import { Store } from '@ngxs/store';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   public loginForm: FormGroup;
   private fb = inject(FormBuilder)
+  private router = inject(Router)
+  private store = inject(Store)
 
   constructor() {
     this.loginForm = this.fb.group({
@@ -17,9 +22,14 @@ export class LoginComponent {
     });
   }
 
+   ngOnInit(): void {
+    this.login()
+  }
+
   public login(): void {
     let loginData = this.loginForm.value;
     console.log(loginData);
+    this.router.navigate(['/home'])
 
   }
 
