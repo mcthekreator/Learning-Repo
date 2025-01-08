c488d81b7267452a576ed90cad9d16ecae3c5ddc
mcthekreator <mcadamsrd@gmail.com>
2025-01-08 03:58:57 +0000

    all js es6 features

--- JavaScript/es6-features/index.js
+++ JavaScript/es6-features/index.js
@@ -1,1 +1,11 @@
 // All javascript es6 features
+//Let and const declarations
+//Arrow functions
+//Template litrerals
+//Destructing Assignments
+//Spead and Rest Operator
+//classes
+//Promises
+//Modules
+//default Parameters
+//Enchanced Object Leiteral syntax
