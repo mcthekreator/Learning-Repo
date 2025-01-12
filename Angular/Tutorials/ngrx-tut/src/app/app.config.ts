8681e1e586aec23ce7902614c9bb941c89dd2ff8
mcthekreator <mcadamsrd@gmail.com>
2025-01-12 14:20:58 +0000

    angular: learning ngrx

--- Angular/Tutorials/ngrx-tut/src/app/app.config.ts
+++ Angular/Tutorials/ngrx-tut/src/app/app.config.ts
@@ -3,7 +3,17 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
-
+import {provideStoreDevtools} from '@ngrx/store-devtools'
+ 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideStore()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideStore(),
+    provideStore({}),
+    provideStore({}),
+    provideStoreDevtools({maxAge: 25, logOnly: false})
+    
+  
+  ]
 };
