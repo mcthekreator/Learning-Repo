33dcff7bd54c10ef1c7a502b38bce3177e5bf027
mcthekreator <mcadamsrd@gmail.com>
2025-01-19 14:42:48 +0000

    angular: learning ngrx

--- Angular/Tutorials/ngrx-tut/src/app/state/students.reducer.ts
+++ Angular/Tutorials/ngrx-tut/src/app/state/students.reducer.ts
@@ -5,5 +5,7 @@ export const initState: ReadonlyArray<StudentsRecords> = [
         name: "Derrick",
         city: "Accra",
         subject: "Python programing",
+        passportDeclaration: "Yes",
+        
     }
 ]
