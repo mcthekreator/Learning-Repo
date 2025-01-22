28883ac12e24421947faef0f25d4bb26f3b29f45
mcthekreator <mcadamsrd@gmail.com>
2025-01-22 21:55:45 +0000

    added auth proj

---
+++ Nest/my-auth/src/user/entities/user.entity.ts
@@ -0,0 +1,20 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({unique: true})
+    username: string;
+
+    @Column({nullable: true, select: true})
+    password: string;
+
+    @Column({nullable: true})
+    name?: string;
+
+    @Column({default:'inactive'})
+    accountStatus: 'active' | 'inactive';
+}
