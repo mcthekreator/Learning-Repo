662f0fe7c0a8fb4dd88edf9d6981fe3a1afa9e24
mcthekreator <mcadamsrd@gmail.com>
2025-01-26 03:15:48 +0000

    started with typescript

--- Nest/nest-auth/src/modules/auth/controllers/auth.controller.ts
+++ Nest/nest-auth/src/modules/auth/controllers/auth.controller.ts
@@ -1,13 +1,12 @@
-import { Controller,Post } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
 
-    constructor(private readonly authService: AuthService) {}    
-
-    @Post('signup')
-    public async creatUser(){
-        return this.authService.createUser();
-    }
+  @Post('signup')
+  public async creatUser() {
+    return this.authService.createUser();
+  }
 }
---
+++ TypeScript/angular-uni/fundamentals/01-why-typescript.js
@@ -0,0 +1,10 @@
+const name = "Typescript bootcamp";
+
+PrintCourseName(name);
+
+PrintCourseName(100);
+PrintCourseName([0,1,2])
+
+function PrintCourseName(name) {
+  console.log(`Course Name: ${courseName}`);
+}
