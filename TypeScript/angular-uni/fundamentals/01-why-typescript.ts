46095b953eac3e186fa7d172985770e80c7f999a
mcthekreator <mcadamsrd@gmail.com>
2025-01-26 03:22:00 +0000

    typescript: change fiel from js to ts

--- TypeScript/angular-uni/fundamentals/01-why-typescript.ts
+++ TypeScript/angular-uni/fundamentals/01-why-typescript.ts
@@ -3,7 +3,7 @@ const courseName = "Typescript bootcamp";
 PrintCourseName(courseName);
 
 PrintCourseName(100);
-PrintCourseName([0,1,2])
+PrintCourseName([0, 1, 2]);
 
 function PrintCourseName(name) {
   console.log(`Course Name: ${name}`);
