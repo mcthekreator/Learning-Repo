09f859c323e83cb6baccf34d0b5fdba8c450cb9a
mcthekreator <mcadamsrd@gmail.com>
2025-01-27 03:26:15 +0000

    typescript: added type to functionm parameter

--- TypeScript/angular-uni/fundamentals/01-why-typescript.ts
+++ TypeScript/angular-uni/fundamentals/01-why-typescript.ts
@@ -3,9 +3,6 @@ const newCourseName = "Typescript bootcamp";
 
 PrintCourseName(newCourseName);
 
-PrintCourseName(100);
-PrintCourseName([0, 1, 2]);
-
 function PrintCourseName(name: string) {
   console.log(`Course Name: ${name}`);
 }
