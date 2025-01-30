9e0b225d1cda860294b403a2fa4d18cdfe195ea7
mcthekreator <mcadamsrd@gmail.com>
2025-01-30 04:36:21 +0000

    js: interview prep, basics

---
+++ JavaScript/interview prep/script.js
@@ -0,0 +1,20 @@
+// Q1: What is javascript?
+// Ans: A programming language used for converting static web pages to interactive and synamic web pages 
+
+// Q2: What are datatypes in Javascript?
+//Ans: A data type determins the type of values that can be stored in a variable 
+
+//Q3: Types of Primitive and  Non-primitive datatypes
+
+// Premitives 
+//1. Numbers
+//2. strings
+//3. Boolean
+//4. Undefined
+//5. Null
+
+//Non-primitives  
+//1. Object
+//2. Array
+//3. Functions
+
