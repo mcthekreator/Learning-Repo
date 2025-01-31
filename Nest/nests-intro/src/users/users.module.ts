5c76dfe091a90194386681ba2a591bafe0989d05
mcthekreator <mcadamsrd@gmail.com>
2025-01-31 10:15:35 +0000

    nest: created controllers with cli

--- Nest/nests-intro/src/users/users.module.ts
+++ Nest/nests-intro/src/users/users.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
 
 @Module({
 
-    controllers: []
+    controllers: [UsersController]
 })
 export class UsersModule {}
