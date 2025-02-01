1a7b06c132e07e5ae96e4a7403e3e178a1789568
mcthekreator <mcadamsrd@gmail.com>
2025-02-01 10:37:05 +0000

    nest: developed post request sample

--- Nest/nests-intro/src/users/users.controller.ts
+++ Nest/nests-intro/src/users/users.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Put, Delete } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
     @Get()
     public getUsers(){
         return 'Get users';
+    }
+
+
+    @Post() 
+    public createUsers(){
+        return "you send a post request to this endpoint"
+
     }
 }
