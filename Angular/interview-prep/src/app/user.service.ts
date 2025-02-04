add01884b3fb662adcf1c53424081014645870bf
mcthekreator <mcadamsrd@gmail.com>
2025-02-04 22:20:15 +0000

    interview prep

---
+++ Angular/interview-prep/src/app/user.service.ts
@@ -0,0 +1,15 @@
+import {Injectable} from '@angular/core'
+import {HttpClient} from '@angular/common/http'
+import {Observable} from 'rxjs'
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UserService {
+
+    constructor(private http: HttpClient ){}
+
+    public getData():Observable <any>{
+        return this.http.get("https://dummyjson.com/products")
+    }
+}
