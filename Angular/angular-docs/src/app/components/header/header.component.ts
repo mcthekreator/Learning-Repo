20a79e2e9a03df4e36d45b852ceb5259b25bf192
mcthekreator <mcadamsrd@gmail.com>
2025-02-17 23:29:01 +0000

    angular: added template to see which one will render first

--- Angular/angular-docs/src/app/components/header/header.component.ts
+++ Angular/angular-docs/src/app/components/header/header.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   imports: [],
+  template: "<h1>My world</h1>",
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
