10a5b7c71678a93ed3acff9028164b7ef542d8ce
mcthekreator <mcadamsrd@gmail.com>
2025-02-17 23:29:31 +0000

    angular: removed template in metadata

--- Angular/angular-docs/src/app/components/header/header.component.ts
+++ Angular/angular-docs/src/app/components/header/header.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   imports: [],
-  template: "<h1>My world</h1>",
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
