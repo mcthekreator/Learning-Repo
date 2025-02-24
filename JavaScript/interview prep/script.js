36a6f27cf8b1898f4e63ce622b980e86292b2fb7
mcthekreator <mcadamsrd@gmail.com>
2025-02-24 21:26:13 +0000

    js: interview prep questions on array

--- JavaScript/interview prep/script.js
+++ JavaScript/interview prep/script.js
@@ -1,61 +1,7 @@
-
-//Question
-// Q1: What is javascript?
-// Ans: A programming language used for converting static web pages to interactive and synamic web pages 
-
-// Q2: What are datatypes in Javascript?
-//Ans: A data type determins the type of values that can be stored in a variable 
-
-//Q3: Types of Primitive and  Non-primitive datatypes
-
-// Premitives 
-//1. Numbers
-//2. strings
-//3. Boolean
-//4. Undefined
-//5. Null
-
-//Non-primitives  
-//1. Object
-//2. Array
-//3. Functions
-
-
-//String methods
-
-//  const greeting = " Hello World"
-// result = greeting.concat("!", " ", "How are you?", greeting);
-// console.log(result);
-// result2 = greeting.substring(0, 3);
-// console.log(result2);
-// result3 = greeting.slice(0, 3);
-// console.log(result3);
-// result4 = greeting.split(" ");
-// console.log(result4);
-// result5 = greeting.replace("Hello", "Hi");
-// console.log(result5);
-// result6 = greeting.trim();
-// console.log(result6);
-
-
-
-
-
-
-//Array methods
-
-const numbers = [1, 2, 3, 4, 5,6];
-// result = numbers.push(6);
-// console.log(result);
-// result2 = numbers.pop();
-// console.log(result2);
-result3 = numbers.unshift();
-console.log(result3);
-result4 = numbers.shift();
-console.log(result4);
-result5 = numbers.filter(function(num){
-    return num > 2;
-})
-console.log(result5);
-let sum = numbers.reduce((acc, num) => acc + num, 0);
-console.log( sum);
+const users = [
+  { firstname: "Daniel", lastname: "Mensah", age: 2 },
+  { firstname: "Daniel", lastname: "Mensah", age: 2 },
+  { firstname: "Ben", lastname: "Lucky", age: 2 },
+  { firstname: "Nolley", lastname: "Dadzie", age: 2 },
+  { firstname: "Bilal", lastname: "Mubarak", age: 2 },
+];
