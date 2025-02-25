aee21e2bc7abb317f72dd19a130bbaa818163117
mcthekreator <mcadamsrd@gmail.com>
2025-02-25 21:30:47 +0000

    js: interview prep questions on array

--- JavaScript/interview prep/script.js
+++ JavaScript/interview prep/script.js
@@ -1,6 +1,6 @@
 const users = [
   { firstname: "Daniel", lastname: "Mensah", age: 2 },
-  { firstname: "Daniel", lastname: "Mensah", age: 2 },
+  { firstname: "mark", lastname: "Mensah", age: 2 },
   { firstname: "Ben", lastname: "Lucky", age: 2 },
   { firstname: "Nolley", lastname: "Dadzie", age: 2 },
   { firstname: "Bilal", lastname: "Mubarak", age: 2 },
