24a7eb23f99b507a2e83765e8e02e4aba0ff5464
mcthekreator <mcadamsrd@gmail.com>
2025-03-02 15:48:12 +0000

    js: interview prep

--- JavaScript/interview prep/script.js
+++ JavaScript/interview prep/script.js
@@ -6,15 +6,58 @@ const users = [
   { firstname: "Bilal", lastname: "Mubarak", age: 2 },
 ];
 
-//  How would you extract the first name of the second user in the list?
-const secondUserFirstName = users[1].firstname;
-console.log(secondUserFirstName);
 
-//How would you find the total number of users in the array?
 
-const totalUsers = users.length; 
-console.log(totalUsers);
+const seconNumber = users[1].firstname
+console.log(seconNumber);
 
-//How would you retrieve an array of all the first names of the users?
-const firstNames = users.map(user => user.firstname); 
-console.log(firstNames); 
+const totalNumber = users.length
+console.log(totalNumber)
+
+
+
+const firstnamesDaniel = users.filter(user => user.firstname === "Ben")
+console.log(firstnamesDaniel)
+
+const firstwithD = users.filter((user) => {
+  if (user.firstname[0] === "D") {
+    console.log(user);
+
+  }
+})
+
+const searchLast = (lastname) => users.filter((user)=>{
+  if(user.lastname === lastname){
+    console.log(user);
+    
+  }
+})
+searchLast("Mensah")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //  How would you extract the first name of the second user in the list?
+// const secondUserFirstName = users[1].firstname;
+// console.log(secondUserFirstName);
+
+// //How would you find the total number of users in the array?
+
+// const totalUsers = users.length; 
+// console.log(totalUsers);
+
+// //How would you retrieve an array of all the first names of the users?
+// const firstNames = users.map(user => user.firstname); 
+// console.log(firstNames); 
