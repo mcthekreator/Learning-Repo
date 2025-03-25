7790b7e9fcfebfcbcd81df8c8f4e02ddb9754d93
mcthekreator <mcadamsrd@gmail.com>
2025-03-25 02:22:57 +0000

    angular: starting with angular components

--- Angular/Tutorials/procadamy/src/app/header/header.component.ts
+++ Angular/Tutorials/procadamy/src/app/header/header.component.ts
@@ -1,4 +1,9 @@
+import { Component } from '@angular/core';
 
+@Component({
+  selector: 'app-header',
+  template:"<header><h1>My First Angular App</h1></header>"
+})
 export class HeaderComponent {
-  constructor() {}
+ 
 }
