8adfeeb288ba2e63d03f0f48370b2a96398358d0
mcthekreator <mcadamsrd@gmail.com>
2025-04-03 20:36:29 +0000

    angular: content projection

---
+++ Angular/Tutorials/ngrx-tut/src/app/reusable-component/reusable-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reusable-component',
+  imports: [],
+  templateUrl: './reusable-component.component.html',
+  styleUrl: './reusable-component.component.scss'
+})
+export class ReusableComponentComponent {
+
+}
