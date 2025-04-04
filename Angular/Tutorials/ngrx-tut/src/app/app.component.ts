d4232d3417f1a84adeba7fdc0728377c2118235b
mcthekreator <mcadamsrd@gmail.com>
2025-04-04 07:10:23 +0000

    angular: learning components in angular

--- Angular/Tutorials/ngrx-tut/src/app/app.component.ts
+++ Angular/Tutorials/ngrx-tut/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReusableComponentComponent } from "./reusable-component/reusable-component.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReusableComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
