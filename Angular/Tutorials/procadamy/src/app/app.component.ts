7e5565f1e6cc8032fdef75bf616c4e2b694fdd22
mcthekreator <mcadamsrd@gmail.com>
2025-04-04 07:50:07 +0000

    angular: learning components in angular

--- Angular/Tutorials/procadamy/src/app/app.component.ts
+++ Angular/Tutorials/procadamy/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
+import { TopHeaderComponent } from "./top-header/top-header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ HeaderComponent],
+  imports: [HeaderComponent, TopHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
