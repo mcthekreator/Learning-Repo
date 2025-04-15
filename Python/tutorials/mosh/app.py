415d532d82662216f9bbb2567ff401136bbe1c73
mcthekreator <mcadamsrd@gmail.com>
2025-04-15 22:15:23 +0000

    python: getting started with python

--- Python/tutorials/mosh/app.py
+++ Python/tutorials/mosh/app.py
@@ -1,2 +1,11 @@
-X = 1
-y = 2
+student_coiunt = 1000
+rating = 4.99
+is_published = True
+course_name = "Python Programing"
+message = """ 
+
+This is a long message
+that spans multiple lines.
+This is a long message
+
+"""
