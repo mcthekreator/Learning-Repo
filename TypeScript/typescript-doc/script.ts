e05081d567191e845ec2314cf1427d659d8b4782
mcthekreator <mcadamsrd@gmail.com>
2025-04-15 22:00:06 +0000

    typescript: starting with typescript

--- TypeScript/typescript-doc/script.ts
+++ TypeScript/typescript-doc/script.ts
@@ -1,17 +1,2 @@
 console.log("Hello, TypeScript!");
 console.log("This is a TypeScript script.");
-console.log("TypeScript is a superset of JavaScript.");
-console.log("TypeScript adds static typing to JavaScript.");
-console.log("TypeScript is developed and maintained by Microsoft.");
-console.log("TypeScript is open-source.");
-console.log("TypeScript is used for large-scale applications.");
-console.log("TypeScript is used by many companies.");
-console.log("TypeScript is used by many developers.");
-console.log("TypeScript is used for front-end and back-end development.");
-console.log("TypeScript is used for web development.");
-console.log("TypeScript is used for mobile development.");
-console.log("TypeScript is used for desktop development.");
-console.log("TypeScript is used for server-side development.");
-console.log("TypeScript is used for cloud development.");
-console.log("TypeScript is used for game development.");
-console.log("TypeScript is used for data science.");
