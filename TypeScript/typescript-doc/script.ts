130fab23202449570c31dde1000afccf7d1573e2
mcthekreator <mcadamsrd@gmail.com>
2025-04-15 22:01:04 +0000

    typescript: starting with typescript

--- TypeScript/typescript-doc/script.ts
+++ TypeScript/typescript-doc/script.ts
@@ -1,4 +1,3 @@
 console.log("Hello, TypeScript!");
 console.log("This is a TypeScript script.");
 console.log("TypeScript is a superset of JavaScript.");
-console.log("TypeScript adds static typing to JavaScript.");
